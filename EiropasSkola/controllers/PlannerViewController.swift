c0e0786e58e2e985965d3bf75a548e25d8cccd0c
Ervīns Malnačs <ervins.malnacs@delfi.lv>
2023-01-16 20:27:33 +0200

    Add PlannerViewController

---
+++ EiropasSkola/controllers/PlannerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlannerViewController.swift
+//  EiropasSkola
+//
+//  Created by Ervins on 16/01/2023.
+//
+
+import UIKit
+
+
+class PlannerViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
