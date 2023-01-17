7cd9b57dd7f0e4818336965effc58209aeb25f53
Ervīns Malnačs <ervins.malnacs@delfi.lv>
2023-01-17 17:39:48 +0200

    Add UIView extension - addSubviews

---
+++ EiropasSkola/resources/UIViewExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewExtension.swift
+//  EiropasSkola
+//
+//  Created by Ervins on 17/01/2023.
+//
+
+import UIKit
+
+
+extension UIView {
+    
+    public func addSubviews(_ views: UIView...) {
+        views.forEach({
+            self.addSubview($0)
+        })
+    }
+    
+}
+
