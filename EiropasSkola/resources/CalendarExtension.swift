edebc20c88a2783b0695063469288f898f8763a7
Ervīns Malnačs <ervins.malnacs@delfi.lv>
2023-02-12 12:24:49 +0200

    Add Latvian holiday calendar extension.

---
+++ EiropasSkola/resources/CalendarExtension.swift
@@ -0,0 +1,28 @@
+//
+//  CalendarExtension.swift
+//  EiropasSkola
+//
+//  Created by Ervins on 08/02/2023.
+//
+
+import Foundation
+
+
+extension Calendar {
+    
+    public enum latvianHolidays: String, CaseIterable {
+        
+        case newYearsDay = "01 01"
+        case workHoliday = "05 01"
+        case independenceDay = "05 04"
+        case ligo = "06 23"
+        case jani = "06 24"
+        case proclamationDay = "11 18"
+        case christmasEve = "12 24"
+        case christmas = "12 25"
+        case secondChristmas = "12 26"
+        case newYearsEve = "12 31"
+        
+    }
+    
+}
