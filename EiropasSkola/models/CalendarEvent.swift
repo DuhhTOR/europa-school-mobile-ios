230347bd202b159226a9fc05baf71c856f15b08f
Ervīns Malnačs <ervins.malnacs@delfi.lv>
2023-03-05 11:17:46 +0200

    Add new CalendarEvent model

---
+++ EiropasSkola/models/CalendarEvent.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarEvent.swift
+//  EiropasSkola
+//
+//  Created by Ervins on 04/03/2023.
+//
+
+import Foundation
+
+
+public struct CalendarEvent {
+    
+    // MARK: - Public variables
+    
+    public let time: Date?
+    public let title: String
+    
+}
+
